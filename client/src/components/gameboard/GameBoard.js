960a7ed2c6613bb6d0626f1a03a01c3c1d96ddd4
James Hamann <hamann.james87@gmail.com>
2022-01-28 22:29:00 -0800

    Working word fetch and win condition: - Endpoint created in server js to fetch word from pre-formatted list of words stored on server - GameBoard component now loads and fetched word from server - Gameboard component haswin condition if word is guessed - Gameboard can reset with newword

--- client/src/components/gameboard/GameBoard.js
+++ client/src/components/gameboard/GameBoard.js
@@ -3,48 +3,132 @@ import { useEffect, useState } from "react"
 import Button from "../layout/Button";
 import Row from "./Row";
 
-const BoardStyled = styled.section``
+const BoardStyled = styled.section`
+  position: relative;
+`;
 
 const GridStyled = styled.div``;
 
+const Correct = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, .9);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  text-transform: uppercase;
+`
+
 export default function GameBoard() {
-  const [grid, updateGrid] = useState([
-    ['w', 'r', 'd', 'g', 'o'],
+  const blankGrid = [
+    ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', '']
-  ]);
-  const [rowNum, updateRowNum] = useState(0);
-  const [rowCount, updateRowCount] = useState(5);
-  const [guess, updateGuess] = useState('');
+  ];
+  const [word, setWord] = useState('');
+  const [loaded, setLoaded] = useState(false);
+  const [grid, setGrid] = useState(Array.from(blankGrid));
+  const [rowNum, setRowNum] = useState(0);
+  const [rowCount, setRowCount] = useState(5);
+  const [guess, setGuess] = useState('');
+  const [correct, setCorrect] = useState(false);
+
+  const updateGuess = (val, index) => {
+    setGuess(val);
+    let newGrid = Array.from(grid);
+    newGrid[index] = [...Array(5)].map((el, pos) => val.charAt(pos) ?? '');
+    setGrid(newGrid);
+  }
+
+  const wipeGrid = (val) => {
+    setGuess(val);
+    let grid = Array.from(blankGrid);
+    grid[0][0] = val;
+    setGrid(grid);
+  }
+
+  const fetchWord = () => {
+    setLoaded(false);
+    fetch('/word')
+      .then(res => res.text())
+      .then(data => {
+        setWord(data);
+        wipeGrid(data.charAt(0));
+        setLoaded(true);
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+  
+  useEffect(() => {
+    fetchWord();
+  },[])
 
   const handleInput = value => {
     // Todo: fire end game event
     if (value.length > 5) return;
-    updateGuess(value);
-    let newGrid = grid;
-    newGrid[rowNum] = [...Array(5)].map((el, pos) => value.charAt(pos) ?? '');
-    updateGrid(newGrid);
+    updateGuess(value, rowNum);
   }
 
   const handleSubmit = e => {
     e.preventDefault();
     // Todo: show warning to user
     if (guess.length < 5) return;
-    updateRowNum(rowNum + 1);
-    updateGuess('');
+
+    if (guess === word) {
+      handleCorrect();
+      return;
+    }
+
+    if (rowNum + 1 === rowCount) {
+      handleFail();
+      return;
+    }
+
+    handleRowChange();
+  }
+
+  const handleCorrect = () => {
+    setCorrect(true);
+  }
+
+  const handleFail = () => {}
+
+  const handleRowChange = () => {
+    setGuess('');
+    setRowNum(rowNum + 1);
+  }
+
+  const handleReset = () => {
+    setRowNum(0);
+    setCorrect(false);
+    fetchWord();
   }
 
   return (
     <BoardStyled>
+      <h2>{word}</h2>
+      {loaded ? <>
       <GridStyled>
-        {[...Array(rowCount)].map((el, i) => <Row rowNum={i} letters={grid[i]} key={i}/>)}
+        {[...Array(rowCount)].map((el, i) => <Row letters={grid[i]} key={i} />)}
       </GridStyled>
       <form onSubmit={handleSubmit}>
         <input maxLength="5" type="text" value={guess} onInput={e => handleInput(e.target.value)} pattern="^[a-zA-Z]+$" />
         <Button type="button" action="submit" color="splash">Guess!</Button>
       </form>
+      {correct && <Correct>
+          <h3>You Win!</h3>
+          <Button type="button" color="accent" onClick={handleReset}>Reset</Button>
+        </Correct>}
+      </>
+      : 'loading'}
     </BoardStyled>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { default: fetch } = require('node-fetch');
 const app = express();
 const port = 3001;
+const { words } = require('./static/words.js');
 
-app.get('/test', (req, res) => {
-  res.send('Hello, World!');
+app.get('/words', (req, res) => {
+  res.json(words);
+});
+
+app.get('/word', (req, res) => {
+  res.send(words[Math.floor(Math.random() * words.length)]);
 });
 
 app.listen(port, () => {
