d30f73ed477b73e0aaa6053ed625edc2b337f0de
James Hamann <hamann.james87@gmail.com>
2022-01-30 23:49:28 -0800

    hook into default port in web server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { default: fetch } = require('node-fetch');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 const { words } = require('./static/words.js');
 const path = require('path');
 
